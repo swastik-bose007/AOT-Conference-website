22636e27e90e489122ba793a06c4b3988bd3831c
swastik <deltaagents4@gmail.com>
2022-10-24 19:32:24 +0530

    routing and basic folder structure

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,24 @@
 <?php
 
+use App\Http\Controllers\AppControllers;
 use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [AppControllers\AppController::class, 'index']);
+Route::get('/about', [AppControllers\AppController::class, 'about']);
+Route::prefix('/all-events')->group(function (){
+    Route::get('/', [AppControllers\AppController::class, 'allEvents']);
+    Route::get('/event', [AppControllers\AppController::class, 'singleEvent']);
 });
+Route::prefix('/all-speakers')->group(function (){
+    Route::get('/', [AppControllers\AppController::class, 'allSpeakers']);
+    Route::get('/speaker', [AppControllers\AppController::class, 'singleSpeaker']);
+});
+Route::get('/call-for-paper', [AppControllers\AppController::class, 'callForPaper']);
+Route::get('/organizer', [AppControllers\AppController::class, 'organizer']);
+Route::get('/contact-us', [AppControllers\AppController::class, 'contact']);
+Route::get('/register', [AppControllers\AppController::class, 'register']);
